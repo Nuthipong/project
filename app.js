4b07f768f8ccac1879386ccc74e3d3595cf7f691
Nuthipong <nuthipong_p@cmu.ac.th>
2022-06-17 02:59:11 +0700

    connect api js

---
+++ app.js
@@ -0,0 +1,28 @@
+const api_url = "https://api.jikan.moe/v3" 
+    
+function searchAnime(event) {
+
+    event.preventDefault();
+
+    const form = new FormData(this);
+    const query = form.get("search");
+    
+    console.log(query);
+
+    
+    fetch(`${api_url}/search/anime?q=${query}&page=1`)
+    .then(res => res.json())
+    .then(updateDom)
+    .catch(err=>console.warn(err.message))
+}
+
+function updateDom(data){
+    data.results.forEach(anime => console.log(anime));
+}
+
+function pageLoaded(){
+    const form = document.getElementById('search_form');
+    form.addEventListener("submit", searchAnime)
+    }
+
+window.addEventListener("load", pageLoaded)
